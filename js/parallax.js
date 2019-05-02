62b3c57a5e3fe7b91b939178bdf5a5f5556a3e34
Thales Ghinis <thales.ghinis@gmail.com>
2019-05-02 02:52:43 -0300

    alterações e parallax

--- js/parallax.js
+++ js/parallax.js
@@ -1,9 +1,12 @@
 $('div.bgParallax').each(function(){
-    var $obj = $(this)
+    var $obj = $(this);
 
     $(window).scroll(function() {
-        var yPos = -($(window).scrollTop() / $obj.data('speed'))
-        var bgpos = '50% '+ yPos + 'px'
-        $obj.css('background-position', bgpos )
-    })
-})
+        var yPos = -($(window).scrollTop() / $obj.data('speed')); 
+
+        var bgpos = '50% '+ yPos + 'px';
+
+        $obj.css('background-position', bgpos );
+
+    }); 
+});
